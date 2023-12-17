8f6504f6da66d289926a5942bb6b452f9fa7b2f7
FlyinPancake <FlyinPancake@users.noreply.github.com>
2023-12-17 23:08:51 +0100

    feat(day_14): :sparkles: added part 2

--- day_14/src/lib.rs
+++ day_14/src/lib.rs
@@ -2,7 +2,7 @@ use std::time::Instant;
 
 use color_eyre::Result;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Tile {
     Empty,
     Square,
@@ -19,14 +19,14 @@ impl Tile {
         }
     }
 }
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Down,
     Left,
     Right,
 }
-
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Map {
     tiles: Vec<Vec<Tile>>,
 }
@@ -73,10 +73,10 @@ impl Map {
         }
     }
 
-    fn roll(mut self, direction: Direction) -> Self {
+    fn roll(&mut self, direction: Direction) {
         match direction {
             Direction::Up => {
-                for x in 0..self.tiles.len() {
+                for x in 0..self.tiles[0].len() {
                     let rounds = self
                         .tiles
                         .iter()
@@ -93,20 +93,97 @@ impl Map {
                         }
                     }
                 }
-                self
             }
-            Direction::Down => todo!(),
-            Direction::Left => todo!(),
-            Direction::Right => todo!(),
+            Direction::Down => {
+                for x in 0..self.tiles[0].len() {
+                    let rounds = self
+                        .tiles
+                        .iter()
+                        .enumerate()
+                        .filter(|(_, row)| row[x] == Tile::Round)
+                        .map(|(y, _)| y)
+                        .rev()
+                        .collect::<Vec<_>>();
+                    for y in rounds {
+                        let mut y = y;
+                        while y < self.tiles.len() - 1
+                            && self.get(x, y + 1).unwrap() == &Tile::Empty
+                        {
+                            self.set(x, y, Tile::Empty);
+                            self.set(x, y + 1, Tile::Round);
+                            y += 1;
+                        }
+                    }
+                }
+            }
+            Direction::Left => {
+                for y in 0..self.tiles.len() {
+                    let rounds = self.tiles[y]
+                        .iter()
+                        .enumerate()
+                        .filter(|(_, tile)| **tile == Tile::Round)
+                        .map(|(x, _)| x)
+                        .collect::<Vec<_>>();
+                    for x in rounds {
+                        let mut x = x;
+                        while x > 0 && self.get(x - 1, y).unwrap() == &Tile::Empty {
+                            self.set(x, y, Tile::Empty);
+                            self.set(x - 1, y, Tile::Round);
+                            x -= 1;
+                        }
+                    }
+                }
+            }
+            Direction::Right => {
+                for y in 0..self.tiles.len() {
+                    let rounds = self.tiles[y]
+                        .iter()
+                        .enumerate()
+                        .filter(|(_, tile)| **tile == Tile::Round)
+                        .map(|(x, _)| x)
+                        .rev()
+                        .collect::<Vec<_>>();
+                    for x in rounds {
+                        let mut x = x;
+                        while x < self.tiles[0].len() - 1
+                            && self.get(x + 1, y).unwrap() == &Tile::Empty
+                        {
+                            self.set(x, y, Tile::Empty);
+                            self.set(x + 1, y, Tile::Round);
+                            x += 1;
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    fn cycle(&mut self) {
+        [
+            Direction::Up,
+            Direction::Left,
+            Direction::Down,
+            Direction::Right,
+        ]
+        .iter()
+        .for_each(|direction| {
+            self.roll(*direction);
+        });
+    }
+
+    fn count(&self, tile: Tile) -> usize {
+        self.tiles
+            .iter()
+            .map(|row| row.iter().filter(|t| **t == tile).count())
+            .sum()
     }
 }
 
 pub fn solve_task_one(#[allow(unused_variables)] input: Vec<String>) -> Result<i32> {
     let start_time = Instant::now();
-    let map = Map::from_lines(input);
+    let mut map = Map::from_lines(input);
     // map.print();
-    let map = map.roll(Direction::Up);
+    map.roll(Direction::Up);
     let sol = map.load(Direction::Up);
     eprintln!("{:?}", Instant::now() - start_time);
     Ok(sol)
@@ -114,8 +191,29 @@ pub fn solve_task_one(#[allow(unused_variables)] input: Vec<String>) -> Result<i
 
 pub fn solve_task_two(#[allow(unused_variables)] input: Vec<String>) -> Result<i32> {
     let start_time = Instant::now();
+    let map = Map::from_lines(input);
+
+    // map.print();
+    let mut map = map;
+    let mut prev = vec![];
+    let mut cycle_start = None;
+    for _ in 0..1_000_000_000 {
+        prev.push(map.clone());
+        map.cycle();
+        if prev.contains(&map) {
+            cycle_start = Some(prev.iter().position(|m| *m == map).unwrap());
+            break;
+        }
+    }
+
+    let cycle_start = cycle_start.unwrap();
+    let cycle_length = prev.len() - cycle_start;
+    let cycle_index = (1_000_000_000 - cycle_start) % cycle_length;
+
+    let map = prev[cycle_start..][cycle_index].clone();
+    let sol = map.load(Direction::Up) as i32;
     eprintln!("{:?}", Instant::now() - start_time);
-    todo!()
+    Ok(sol)
 }
 
 #[cfg(test)]
@@ -159,7 +257,7 @@ mod test {
     fn test_case_two_example() -> Result<()> {
         assert_eq!(
             solve_task_two(get_file(PathBuf::from("inputs/example_2.txt"))?)?,
-            0
+            64
         );
         Ok(())
     }
@@ -168,7 +266,7 @@ mod test {
     fn test_case_two_solve() -> Result<()> {
         assert_eq!(
             solve_task_two(get_file(PathBuf::from("inputs/full.txt"))?)?,
-            0
+            90982
         );
         Ok(())
     }
