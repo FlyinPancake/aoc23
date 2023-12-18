a84d07895f5b0ae12344b48a0399c5f892d3311d
FlyinPancake <FlyinPancake@users.noreply.github.com>
2023-12-18 17:43:06 +0100

    test(day_1): :bug: brought up to current fw

--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -1,8 +1,7 @@
 use color_eyre::{eyre::anyhow, Result};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
-#[allow(dead_code)]
-fn solve_task_one(input: Vec<String>) -> Result<i32> {
+pub fn solve_task_one(input: Vec<String>) -> Result<i32> {
     let lines = input;
 
     let lines = lines
@@ -21,8 +20,7 @@ fn solve_task_one(input: Vec<String>) -> Result<i32> {
     i32::try_from(sol).map_err(|_| anyhow!("Number doesn't fit!"))
 }
 
-#[allow(dead_code)]
-fn solve_task_two(input: Vec<String>) -> Result<i32> {
+pub fn solve_task_two(input: Vec<String>) -> Result<i32> {
     let lines = input
         .into_par_iter()
         .map(|line| {
@@ -54,10 +52,6 @@ fn solve_task_two(input: Vec<String>) -> Result<i32> {
     i32::try_from(sol).map_err(|_| anyhow!("Number doesn't fit!"))
 }
 
-fn main() {
-    unreachable!();
-}
-
 #[cfg(test)]
 mod test {
 
@@ -79,36 +73,32 @@ mod test {
 
     #[test]
     fn test_case_one_example() -> Result<()> {
-        assert_eq!(
-            solve_task_one(get_file(PathBuf::from("inputs/example_1.txt"))?)?,
-            142
-        );
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let file = get_file(cargo_manifest_dir.join("inputs/example_1.txt"))?;
+        assert_eq!(solve_task_one(file)?, 142);
         Ok(())
     }
     #[test]
     fn test_case_one_solve() -> Result<()> {
-        assert_eq!(
-            solve_task_one(get_file(PathBuf::from("inputs/full.txt"))?)?,
-            55017
-        );
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let file = get_file(cargo_manifest_dir.join("inputs/full.txt"))?;
+        assert_eq!(solve_task_one(file)?, 55017);
         Ok(())
     }
 
     #[test]
     fn test_case_two_example() -> Result<()> {
-        assert_eq!(
-            solve_task_two(get_file(PathBuf::from("inputs/example_2.txt"))?)?,
-            281
-        );
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let file = get_file(cargo_manifest_dir.join("inputs/example_2.txt"))?;
+        assert_eq!(solve_task_two(file)?, 281);
         Ok(())
     }
 
     #[test]
     fn test_case_two_solve() -> Result<()> {
-        assert_eq!(
-            solve_task_two(get_file(PathBuf::from("inputs/full.txt"))?)?,
-            53539
-        );
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let file = get_file(cargo_manifest_dir.join("inputs/full.txt"))?;
+        assert_eq!(solve_task_two(file)?, 53539);
         Ok(())
     }
 }
