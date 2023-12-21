e482a8ccbd20795e2c7e9a9c63a806db6b38bc7b
FlyinPancake <FlyinPancake@users.noreply.github.com>
2023-12-21 10:03:10 +0100

    feat(day_21): :tada: added day

---
+++ day_21/src/lib.rs
@@ -0,0 +1,68 @@
+use std::time::Instant;
+
+use color_eyre::Result;
+
+pub fn solve_task_one(#[allow(unused_variables)] input: Vec<String>) -> Result<i32> {
+    let start_time = Instant::now();
+    eprintln!("{:?}", Instant::now() - start_time);
+    todo!()
+}
+
+pub fn solve_task_two(#[allow(unused_variables)] input: Vec<String>) -> Result<i32> {
+    let start_time = Instant::now();
+    eprintln!("{:?}", Instant::now() - start_time);
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+
+    use color_eyre::Result;
+    use std::{
+        fs::File,
+        io::{BufRead, BufReader},
+        path::PathBuf,
+    };
+
+    fn get_file(filename: PathBuf) -> Result<Vec<String>> {
+        let file = File::open(filename)?;
+        let reader = BufReader::new(file);
+        let lines: std::result::Result<Vec<String>, _> = reader.lines().collect();
+        Ok(lines?)
+    }
+
+    use crate::{solve_task_one, solve_task_two};
+    #[ignore = "Not implemented yet"]
+    #[test]
+    fn test_case_one_example() -> Result<()> {
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let file = get_file(cargo_manifest_dir.join("inputs/example_1.txt"))?;
+        assert_eq!(solve_task_one(file)?, 0);
+        Ok(())
+    }
+
+    #[ignore = "Not implemented yet"]
+    #[test]
+    fn test_case_one_solve() -> Result<()> {
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let file = get_file(cargo_manifest_dir.join("inputs/full.txt"))?;
+        assert_eq!(solve_task_one(file)?, 0);
+        Ok(())
+    }
+    #[ignore = "Not implemented yet"]
+    #[test]
+    fn test_case_two_example() -> Result<()> {
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let file = get_file(cargo_manifest_dir.join("inputs/example_1.txt"))?;
+        assert_eq!(solve_task_two(file)?, 0);
+        Ok(())
+    }
+    #[ignore = "Not implemented yet"]
+    #[test]
+    fn test_case_two_solve() -> Result<()> {
+        let cargo_manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let file = get_file(cargo_manifest_dir.join("inputs/full.txt"))?;
+        assert_eq!(solve_task_two(file)?, 0);
+        Ok(())
+    }
+}
